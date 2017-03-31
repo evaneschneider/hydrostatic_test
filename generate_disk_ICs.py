c0cb4b9d885b136e984cafca90d9f7a6a144579f
Evan Schneider <evan@brown.as.arizona.edu>
2017-03-31 13:05:10 -0700

    new ICs generator

--- generate_disk_ICs.py
+++ generate_disk_ICs.py
@@ -19,20 +19,24 @@ G = 6.67259e-8 # in cm^3 g^-1 s^-2
 mp = 1.67e-24 # proton mass in grams
 G = G / l_s**3 * m_s * t_s**2 # in kpc^3 / M_sun / kyr^2
 KB = 1.3806e-16 # boltzmann constant in cm^2 g / s^2 K
-M_vir = 1e12
+M_vir = 1.0e12
 M_d = 6.5e10
 M_h = M_vir - M_d
-R_vir = 261 # MW viral radius in kpc
-c_vir = 20 
+R_vir = 261.0 # MW viral radius in kpc
+c_vir = 20.0 
 R_h = R_vir / c_vir # halo scale radius in kpc
 R_d = 3.5 # stellar disk scale length in kpc
 Z_d = 3.5/5.0 # disk scale height in kpc
 R_g = 2*R_d # gas disk scale length in kpc
-T = 1e4 # gas temperature, 10^4 K
+T = 1.0e5 # gas temperature, 10^4 K
 cs = np.sqrt(KB*T/(0.6*mp))*t_s/l_s # isothermal sound speed
 v_to_kmps = l_s/t_s/100000
 kmps_to_kpcpkyr = 1.0220122e-6
 gamma = 1.001
+
+
+dnamedata='/Volumes/data1/evan/data/disk_3D/128/hdf5/'
+dname='./128'
 
 # function for halo concentration, NFW profile
 def func(xx):
@@ -41,9 +45,9 @@ def func(xx):
 i = 0
 
 # set up the grid
-nx = 128
-ny = 128
-nz = 128
+nx = 128 
+ny = 128 
+nz = 128 
 xmin, xmax = -20,20
 ymin, ymax = -20,20
 zmin, zmax = 0,10
@@ -60,93 +64,103 @@ r_pos = np.sqrt(x_pos**2 + y_pos**2)
 
 # define the surface density distribution (exponential disk)
 sigma = 0.25*M_d*np.exp(-r_pos/R_g) / (2*np.pi*R_g*R_g)
-sigma_e = 0.25*M_d*np.exp(-r/R_g) / (2*np.pi*R_g*R_g) # exact solution for comparison
+sigma_e = 0.25*M_d*np.exp(-r/R_g) / (2*np.pi*R_g*R_g)
 
 # define phi_0_h
 phi_0_h = G * M_h / (R_h * func(c_vir))
 B_h = phi_0_h / cs**2 
 
-# NFW halo potential with Phi_0_h divided out
+# NFW halo potential with Phi_0 divided out
 def phi_halo(z, r, r_h):
   return -np.log(1 + np.sqrt(r**2 + z**2)/r_h)/(np.sqrt(r**2 + z**2)/r_h)
 
-# define phi_0_h
+# define phi_0_d
 phi_0_d = G * M_d / R_d
 B_d = phi_0_d / cs**2 
 
-# Miyamoto-Nagai disk with Phi_0_d divided out
+# Miyamoto-Nagai disk with Phi_0 divided out
 def phi_disk(z, r, r_d, z_d):
-  return -1.0 / np.sqrt((r/r_d)**2 + (1 + np.sqrt((z/r_d)**2 + (z_d/r_d)**2))**2)
+  return - 1.0 / np.sqrt((r/r_d)**2 + (1 + np.sqrt((z/r_d)**2 + (z_d/r_d)**2))**2)
 
 # set the constant in the exponential argument
 # this is an array with a unique value for each r_pos
 A_h = phi_halo(0, r_pos, R_h)
 A_d = phi_disk(0, r_pos, R_d, Z_d)
 
+def gr_halo(z, r, G, M_h, R_h):
+  return 
 
-# radial acceleration due to the disk potential
 def gr_disk(z, r, G, M_d, R_d, z_d):
   return G * M_d * r / (r**2 + (R_d + np.sqrt(z*z + z_d*z_d))**2)**(1.5)
 
-# integrand with just halo potential
+
 def integrand_h(z, r, A, B, r_h):
   return np.exp(B * (A + np.log(1+(np.sqrt(r**2 + z**2)/r_h))/(np.sqrt(r**2 + z**2)/r_h)))
-# integrand with just disk potential
 def integrand_d(z, r, A, B, r_d, z_d):
   return np.exp(B * (A + 1.0 / np.sqrt((r/r_d)**2 + (1 + np.sqrt((z/r_d)**2 + (z_d/r_d)**2))**2)))
-# integrand with both potentials
 def integrand(z, r, A_h, A_d, B_h, B_d, r_h, r_d, z_d):
   return np.exp(B_h*(A_h + np.log(1+(np.sqrt(r**2 + z**2)/r_h))/(np.sqrt(r**2 + z**2)/r_h)) + B_d*(A_d + 1.0 / np.sqrt((r/r_d)**2 + (1 + np.sqrt((z/r_d)**2 + (z_d/r_d)**2))**2)))
 
-# solve the integral over z for each r
+
 sol = np.ones_like(r_pos)
 for ii in range(0, nx):
   for jj in range(0, ny):
     #sol[ii,jj],err = quad(integrand_h, 0, 10, args=(r_pos[ii,jj],A_h[ii,jj],B_h,R_h))
-    #sol[ii,jj],err = quad(integrand_d, 0, 10, args=(r_pos[ii,jj],A_d[ii,jj],B_d,R_d,Z_d))
-    sol[ii,jj],err = quad(integrand, 0, 10, args=(r_pos[ii,jj],A_h[ii,jj],A_d[ii,jj],B_h,B_d,R_h,R_d,Z_d))
+    sol[ii,jj],err = quad(integrand_d, 0, 10, args=(r_pos[ii,jj],A_d[ii,jj],B_d,R_d,Z_d))
+    #sol[ii,jj],err = quad(integrand, 0, 10, args=(r_pos[ii,jj],A_h[ii,jj],A_d[ii,jj],B_h,B_d,R_h,R_d,Z_d))
 
-# central density comes from the surface density distribution
+
 rho_0 = 0.5 * sigma / sol
-
-
 d_temp = np.empty([nx, ny, 2*nz])
-d = np.empty([nx, ny, nz//2])
 
 # set densities (symmetric about z=0)
 for kk in range(0, nz):
   #d_temp[:,:,nz+kk] = d_temp[:,:,nz-kk-1] = rho_0*np.exp(B_h*(A_h - phi_halo(z[kk],r_pos,R_h)))
-  #d_temp[:,:,nz+kk] = d_temp[:,:,nz-kk-1] = rho_0*np.exp(B_d*(A_d - phi_disk(z[kk],r_pos,R_d,Z_d)))
-  d_temp[:,:,nz+kk] = d_temp[:,:,nz-kk-1] = rho_0*np.exp(B_h*(A_h - phi_halo(z[kk],r_pos,R_h)) + B_d*(A_d - phi_disk(z[kk],r_pos,R_d,Z_d)))
+  d_temp[:,:,nz+kk] = d_temp[:,:,nz-kk-1] = rho_0*np.exp(B_d*(A_d - phi_disk(z[kk],r_pos,R_d,Z_d)))
+  #d_temp[:,:,nz+kk] = d_temp[:,:,nz-kk-1] = rho_0*np.exp(B_h*(A_h - phi_halo(z[kk],r_pos,R_h)) + B_d*(A_d - phi_disk(z[kk],r_pos,R_d,Z_d)))
 
-# bin the mass every 4 cells in z to ensure mass conservation
+d = np.empty([nx, ny, nz/2])
 v_cell = dx*dy*dz
 M = d_temp*v_cell
 dz_new = 4*dz
 v_cell_new = dx*dy*dz_new
 for kk in range(0, 2*nz, 4):
-  d[:,:,kk//4] = (M[:,:,kk] + M[:,:,kk+1] + M[:,:,kk+2] + M[:,:,kk+3])/v_cell_new
+  d[:,:,kk/4] = (M[:,:,kk] + M[:,:,kk+1] + M[:,:,kk+2] + M[:,:,kk+3])/v_cell_new
 
 # now redefine everything in terms of the new z array
-nz = int(nz/2)
+nz = nz/2
 dz = dz_new
-z = np.linspace(-zmax+0.5*dz,zmax-0.5*dz,nz)  
+z = np.linspace(-zmax+0.5*dz,zmax-0.5*dz,nz)
 
 """
-# old version of setting densities (without mass conservation)
-d = np.empty([nx,ny,2*nz])
-for kk in range(0, nz):
-  d[:,:,nz+kk] = d[:,:,nz-kk-1] = rho_0*np.exp(B_h*(A_h - phi_halo(z[kk],r_pos,R_h)) + B_d*(A_d - phi_disk(z[kk],r_pos,R_d,Z_d)))
-# now redefine everything in terms of the new z array
-nz = int(2*nz)
-z = np.linspace(-zmax+0.5*dz,zmax-0.5*dz,nz)  
+xi=64
+yi=64
+zz = np.linspace(0,dz,1001)
+#plt.plot(zz, integrand_h(zz, r_pos[xi,yi], A_h[xi,yi], B_h, R_h))
+#plt.plot(zz, integrand_d(zz, r_pos[xi,yi], A_d[xi,yi], B_d, R_d, Z_d))
+#plt.plot(zz, np.exp(B_h*(A_h[xi,yi] - phi_halo(zz,r_pos[xi,yi],R_h))))
+#plt.plot(zz, np.exp(B_d*(A_d[xi,yi] - phi_disk(zz,r_pos[xi,yi],R_d,Z_d))))
+plt.plot(zz, np.exp(B_h*(A_h[xi,yi] - phi_halo(zz,r_pos[xi,yi],R_h)) + B_d*(A_d[xi,yi] - phi_disk(zz,r_pos[xi,yi],R_d,Z_d))))
+plt.yscale('log')
+plt.show()
 """
-
 # don't let densities get too low
-lowd = np.where(d < 1)
+lowd = np.where(d < 1.0)
 d[lowd] = 1.0
 
-# set the rest of the disk properties
+
+# calculate pressure derivs
+P = d*(cs**2)/gamma
+dPdx = np.gradient(P, dx, axis=0)
+dPdy = np.gradient(P, dy, axis=1)
+#dPdx[:,:,:] = 0.0
+#dPdy[:,:,:] = 0.0
+#for kk in range(0, nz):
+#  plt.plot(dPdy[64,:,kk])
+#  plt.ylim(-0.02, 0.02)
+#  plt.text(2,0.0008,str(kk))
+#  plt.show()
+
 mx = np.empty([nx, ny, nz])
 my = np.empty([nx, ny, nz])
 mz = np.empty([nx, ny, nz])
@@ -158,76 +172,41 @@ for kk in range(0, nz):
       xp = x[ii]
       yp = y[jj]
       zp = z[kk]
+      rp = r_pos[ii,jj] 
       dp = d[ii,jj,kk]
-      rp = np.sqrt(xp*xp + yp*yp)
-      phi = np.arctan2(yp, xp)
-      a_d = gr_disk(zp, rp, G, M_d, R_d, Z_d) # centripetal acceleration due to disk potential
-      a_h = 0.0 # haven't calculated this yet
-      a = a_d + a_h
-      #circular velocity from radial acceleration
+      #calculate radial acceleration due to potential
+      a_d = gr_disk(zp, rp, G, M_d, R_d, Z_d)
+      #calculate radial pressure gradient
+      dPdr = xp*dPdx[ii,jj,kk]/rp + yp*dPdy[ii,jj,kk]/rp
+      #circular velocity from combined radial acceleration and pressure gradient
+      a = a_d + dPdr/dp
       v = np.sqrt(rp*a)
       vr[ii,jj,kk] = v
-      vx = -np.sin(phi)*v
-      vy = np.cos(phi)*v
-      vx = 0.0
-      vy = 0.0
+      #vx = -np.sin(phi)*v
+      #vy = np.cos(phi)*v
+      vx = -(yp/rp)*v
+      vy = (xp/rp)*v
+      #vx = 0.0
+      #vy = 0.0
       vz = 0.0
       mx[ii,jj,kk] = dp*vx
       my[ii,jj,kk] = dp*vy
       mz[ii,jj,kk] = dp*vz
-      # pressure assuming isothermal
-      P = dp*(cs**2)/(gamma)
-      E[ii,jj,kk] = P/(gamma-1.0) + 0.5*dp*(vx*vx + vy*vy)
+      E[ii,jj,kk] = P[ii,jj,kk]/(gamma-1.0) + 0.5*dp*(vx*vx + vy*vy)
 
-# write the array to an hdf5 file
-#dname='/Volumes/data1/evan/data/disk_3D/128'
-dname='.'
+print(np.max(vr)*v_to_kmps)
 
-dims = [nx,ny,nz]
-f = h5py.File(dname+'/hdf5/0.h5', 'w')
-f.attrs.create("gamma", gamma)
-f.attrs.create("t", 0.0)
-f.attrs.create("n_step", 0)
-f.attrs.create("dims", dims)
-f.create_dataset('density', data=d)
-f.create_dataset('momentum_x', data=mx)
-f.create_dataset('momentum_y', data=my)
-f.create_dataset('momentum_z', data=mz)
-f.create_dataset('Energy', data=E)
-f.close()
-
-# to get a single column for a 1D test, use this code
-# note that you'll have to set the correct value of r_disk
-# in the 'calc_g_1D' function in hydro_cuda.cu
-"""
-xi,yi = 32,32
-print(r_pos[xi,yi])
-dims = [nz,1,1]
-# write the array to an hdf5 file
-f = h5py.File(dname+'/hdf5/0.h5', 'w')
-f.attrs.create("gamma", gamma)
-f.attrs.create("t", 0.0)
-f.attrs.create("n_step", 0)
-f.attrs.create("dims", dims)
-f.create_dataset('density', data=d[xi,yi,:])
-f.create_dataset('momentum_x', data=mx[xi,yi,:])
-f.create_dataset('momentum_y', data=my[xi,yi,:])
-f.create_dataset('momentum_z', data=mz[xi,yi,:])
-f.create_dataset('Energy', data=E[xi,yi,:])
-f.close()
-"""
-# make a plot of the density projections
 pdz = np.sum(d*dz, axis=2)
 pdy = np.sum(d*dy, axis=1)
 log_pdz = np.log10(pdz)
 log_pdy = np.log10(pdy)
 
-plt.plot(r_pos, pdz, 'ro')
-plt.plot(r, sigma_e)
-plt.xlim([0,30])
+#plt.plot(r_pos, pdz, 'ro')
+#plt.plot(r, sigma_e)
+#plt.xlim([0,30])
 #plt.ylim([1e5,1e7])
 #plt.yscale('log')
-plt.show()
+#plt.show()
 
 
 #make color image
@@ -269,7 +248,7 @@ H = H.T
 
 #set the projected density scale
 rho_pd_min = np.min(log_pdy) 
-rho_pd_max = 1.5*np.max(log_pdy)
+rho_pd_max = 1.1*np.max(log_pdy)
 
 #produce baseline density plot
 rpd = (log_pdy - rho_pd_min)/(rho_pd_max - rho_pd_min)
@@ -313,8 +292,32 @@ a1.hlines(200, 0, 400, color='white')
 a1.hlines(1, 0, 400, color='white')
 plt.savefig(dname+'/png/'+str(i)+'.png', dpi=300)
 plt.close(fig)
+"""
+for zi in range(0, nz):
+  fig = plt.figure(figsize=(8,4), dpi=100)
+  a0 = plt.axes([0,0,0.5,1])
+  vx_plot = mx[:,:,zi]/d[:,:,zi]
+  vy_plot = my[:,:,zi]/d[:,:,zi]
+  plt.imshow(vx_plot.T, origin='lower', interpolation='none')
+  plt.axis('off')
+  a0.text(3, 35, r'$v_x$', color='white')
+  a1 = plt.axes([0.5,0,0.5,1])
+  plt.imshow(vy_plot.T, origin='lower', interpolation='none')
+  plt.axis('off')
+  a1.text(3, 35, '$v_y$', color='white')
+  plt.savefig(dname+'/png/v_'+str(i)+str(zi)+'.png', dpi=300)
+  plt.close(fig)
+"""
 
-
-
-
-
+# write the array to an hdf5 file
+f = h5py.File(dnamedata+'0.h5.0', 'w')
+f.attrs['gamma'] = gamma
+f.attrs['t'] = 0.0
+f.attrs['dt'] = 0.1
+f.attrs['n_step'] = 0
+f.create_dataset('density', data=d)
+f.create_dataset('momentum_x', data=mx)
+f.create_dataset('momentum_y', data=my)
+f.create_dataset('momentum_z', data=mz)
+f.create_dataset('Energy', data=E)
+f.close()
